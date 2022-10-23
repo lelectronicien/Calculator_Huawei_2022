7aa8b9d4590928534e91210ac7a510cc3188e518
Rick Tienson Lubin <ricklubin92@gmail.com>
2022-10-23 14:45:38 +0300

    script

---
+++ script.js
@@ -0,0 +1,21 @@
+//let's display the values of our operands when we click on the buttons.
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.onclick = () => {
+      if (button.id == "clear") {
+        display.innerText = "";
+      } else if (button.id == "backspace") {
+        let string = display.innerText.toString();
+        display.innerText = string.substr(0, string.length - 1);
+      } else if (display.innerText != "" && button.id == "equal") {
+        display.innerText = eval(display.innerText);
+      } else if (display.innerText == "" && button.id == "equal") {
+        display.innerText = "No digit, no result!";
+        setTimeout(() => (display.innerText = ""), 2000);
+      } else {
+        display.innerText += button.id;
+      }
+    };
+  });
