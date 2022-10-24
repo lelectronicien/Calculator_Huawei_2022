35df5e75255e33d57ad76ba3c364903f78a9a228
Rick Tienson Lubin <ricklubin92@gmail.com>
2022-10-24 11:36:30 +0300

    script

--- script.js
+++ script.js
@@ -2,28 +2,33 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
-buttons.forEach((button) => {
-    button.onclick = () => {
-      if (button.id == "clear") {
-        display.innerText = "";
-      } 
-      else if (button.id == "backspace") {
-        let string = display.innerText.toString();
-        display.innerText = string.substr(0, string.length - 1);
-      } 
-      else if (display.innerText != "" && button.id == "equal") {
-        display.innerText = eval(display.innerText);
-      } 
-      else if (display.innerText == "" && button.id == "equal") {
-        display.innerText = "No digit, no result!";
-        setTimeout(() => (display.innerText = ""), 2000);
-      } 
-      else if (button.id == "%"){
-        display.innerText = parseFloat(display.innerText)/100;
-      }
-      
-      else {
-        display.innerText += button.id;
-      }
-    };
-  });
+buttons.forEach(button => 
+    {
+        button.onclick = () => 
+        {
+            if (button.id == "clear") {
+                display.innerText = "";
+            }
+
+            else if (button.id == "backspace") {
+                let string = display.innerText.toString();
+                display.innerText = string.substr(0, string.length - 1);
+            }
+
+            else if (display.innerText != "" && button.id == "equal") {
+                display.innerText = eval(display.innerText);
+            }
+
+            else if (display.innerText == "" && button.id == "equal") {
+                display.innerText = "No digit, no result!";
+            }
+
+            else if (button.id == "%"){
+                display.innerText = parseFloat(display.innerText)/100;
+            }
+            
+            else {
+                display.innerText += button.id;
+            }
+        };
+    });
