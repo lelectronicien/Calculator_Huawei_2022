c0376419cdbd232e860744ee19cacfe4dd941872
Rick Tienson Lubin <ricklubin92@gmail.com>
2022-10-24 12:04:52 +0300

    script comment

--- script.js
+++ script.js
@@ -6,27 +6,29 @@ BUTTONS.forEach(button =>
     {
         button.onclick = () => 
         {
+            //clear the display screen
             if (button.id == "clear") {
                 DISPLAY.innerText = "";
             }
-
+            //clear the display screen one caracter after an other
             else if (button.id == "backspace") {
                 let string = DISPLAY.innerText.toString();
-                DISPLAY.innerText = string.substr(0, string.length - 1);
+                DISPLAY.innerText = string.substr(0, string.length-1);
             }
-
+            //give the results of the input
             else if (DISPLAY.innerText != "" && button.id == "equal") {
                 DISPLAY.innerText = eval(DISPLAY.innerText);
             }
-
+            //When the user didn't enter any digits
             else if (DISPLAY.innerText == "" && button.id == "equal") {
                 DISPLAY.innerText = "No digit, no result!";
             }
-
+            //Give the percent of the input
             else if (button.id == "%"){
                 DISPLAY.innerText = parseFloat(DISPLAY.innerText)/100;
             }
-            
+            /*continue to display the digits when 
+            the user enter more than one time*/
             else {
                 DISPLAY.innerText += button.id;
             }
